abd99e9f9e40d2e9c38cc625940a16edee098dd2
marf <1fabrism@gmail.com>
2020-12-09 17:42:25 +0100

    day 9 start

---
+++ 2020/day9.py
@@ -0,0 +1,18 @@
+"""
+
+"""
+
+# PART 1:
+with open("day9_input.txt", 'r') as file:
+    input = file.readlines()
+
+
+
+"""
+
+"""
+
+# PART 2:
+with open("day9_input.txt", 'r') as file:
+    input = file.readlines()
+
